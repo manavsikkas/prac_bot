e958f97da86f6ae830b61a41b8d6cab92cb38e2e
Manav Sikka <manavsikka5@gmail.com>
2026-02-02 15:01:51 +1100

    Remove old RViz configuration and update simulation launch files; add lidar and joint state publisher configurations.

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -11,69 +11,70 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
     package_name = 'prac_bot'
-
-    # Include the robot_state_publisher launch file
+    
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-        )]), launch_arguments={'use_sim_time': 'true'}.items()
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
+            launch_arguments={'use_sim_time': 'true'}.items()
+            )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package.
+    default_world = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
     )
 
-    # Include the Gazebo Harmonic launch file (ros_gz_sim)
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-        )]),
-        launch_arguments={'gz_args': '-r render_engine:ogre2 empty.sdf'}.items()
-    )
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+            launch_arguments={'gz_args': ['-r -v4 --render-engine ogre2 ', LaunchConfiguration('world')], 'on_exit_shutdown': 'true'}.items()
+        )
 
-    # Spawn the robot in Gazebo using ros_gz_sim create
+    # Spawn the robot into Gazebo
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
+        arguments=['-topic', 'robot_description', '-name', 'prac_bot', '-z', '0.1'],
+        output='screen'
+    )
+
+    bridge_params = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
         arguments=[
-            '-topic', 'robot_description',
-            '-name', 'prac_bot',
-            '-z', '0.1'
-        ],
-        output='screen'
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
     )
-    bridge_config = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['--ros-args', '-p', f'config_file:={bridge_config}'],
-        output='screen'
-    )
+
     joint_state_pub = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
         parameters=[{'use_sim_time': True}],
-        output='screen',
+        output='screen'
     )
+
     rviz2 = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', [os.path.join(get_package_share_directory(package_name), 'config', 'prac_bot.rviz')]],
-        parameters=[{'use_sim_time': True}],
-        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'prac_bot.rviz')],
+        output='screen'
     )
-    lidar_frame_fix = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'lidar', 'prac_bot/base_link/lidar'],
-        output='screen',
-    )
-    # Launch them all!
+
     return LaunchDescription([
+        SetEnvironmentVariable('LIBGL_ALWAYS_SOFTWARE', '1'),
+        SetEnvironmentVariable('MESA_GL_VERSION_OVERRIDE', '3.3'),
+        world_arg,
         rsp,
         gazebo,
         spawn_entity,
-        bridge,
+        ros_gz_bridge,
         joint_state_pub,
         rviz2,
-        lidar_frame_fix,
     ])
